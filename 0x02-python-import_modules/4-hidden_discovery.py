895f7e7fc5e1655cc71a20f42a54a809b84825ef
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-06-08 14:17:47 -0700

    works fine

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for name in sorted(dir(hidden_4)):
+    for name in dir(hidden_4):
         if name[:2] != '__':
             print("{}".format(name))
