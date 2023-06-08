e26a0430a25dcac4c9de62ed31b8cd4f7413ea03
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-06-08 14:16:34 -0700

    fixing error it was a typo on directory to python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
 
-    for name indir(hidden_4):
-        if name[:2] != "__":
+    for name in sorted(dir(hidden_4)):
+        if name[:2] != '__':
             print("{}".format(name))
-
