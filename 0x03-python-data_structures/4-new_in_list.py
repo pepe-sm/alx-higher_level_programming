8944d7daf0b068e9b7460b4c6e7ab49763850d56
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-06-11 15:37:39 -0700

    modify a list without making changes to the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    list_copy = my_list[:]
+    if 0 <= idx and idx < len(list_copy):
+        list_copy[idx] = element
+    return list_copy
