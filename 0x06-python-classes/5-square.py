4dc8fba72525a4e7fdf1972281e4dff4b67f8fd0
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-06-27 13:06:55 -0700

    printin a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"5-square.py define"
+
+
+class Square:
+    """class Square"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise valueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                print("#" * self.__size)
