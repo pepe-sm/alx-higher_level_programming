3ae8426af484a6763ba75b48b7a7adcdb4065acc
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-07-03 11:36:01 -0700

    print message in deletion of an instance

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+""" Defines Rectangle class """
+
+
+class Rectangle:
+    """ Defines a rectangle """
+
+    def __init__(self, width=0, height=0):
+        """ Initializes a rectangle """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Sets the width rectangle """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Height getter """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Sets the height rectangle """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Returns the area rectangle """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ Returns the perimeter new rectangle """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return (self.__height + self.__width) * 2
+
+    def __str__(self):
+        """ Prints the rectangle """
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        return (("#" * self.__width + "\n") * self.__height).strip("\n")
+
+    def __repr__(self):
+        """ Returns the representation of rectangle """
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        """ when instance of rectanle is deleted prints messae """
+        print("Bye rectangle...")
