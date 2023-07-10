2b48b662b5b6f8b6063695922801b5959ffd7313
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-07-10 14:15:22 -0700

    create class square and inherit value validator from BaseGeometry and area from Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 10-square module """
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
