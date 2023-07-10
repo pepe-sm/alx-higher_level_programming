40c9028449ff94cccabf5ea808215a13d77f4dec
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-07-10 14:34:09 -0700

    writes a new attribute to an object

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-    101-add_attribute module
-"""
+""" 101-add_attribute module """
 
 
 def add_attribute(obj, attr, value):
+    """ if Possible add attribute to class  """
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, attr, value)
