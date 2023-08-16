9ce351a422ce3c69212af22b3546e4870deae2e0
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-08-16 13:06:34 -0700

    create new user and grant all previlaes

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- Create mysql server user user_0d_1 if it doesnt exist identified by password
--- Grant all privilagges to created user and flush
+-- creates the MySQL server user ive user all privilaes
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
