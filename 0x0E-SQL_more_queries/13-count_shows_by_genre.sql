cdd846a4bd3e9c120f93f9fcccce91e110cc68d0
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-08-16 14:32:55 -0700

    occurences of shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- displays number of shows in specific genre
+SELECT
+    name AS genre,
+    COUNT(*) AS number_of_shows
+FROM
+    tv_show_genres
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY
+    tv_genres.name
+ORDER BY
+    number_of_shows DESC;
