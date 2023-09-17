90a1a57d6069b4fd816a8aa74039448834e13cbd
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-09-17 02:39:55 -0700

    filter states tat starts with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" select names with N """
+import MySQLdb
+from sys import argv
+
+
+def filter():
+    """function filter
+    """
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1][0] == 'N':
+            print(row)
+
+    db.close()
+
+if __name__ == '__main__':
+    filter()
