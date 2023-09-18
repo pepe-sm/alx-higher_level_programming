68abedafcded94336308195ebc637c4944742bc7
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-09-18 04:11:43 -0700

    fetc allstates usin sqalcemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine, select
+"""A script that lists all State
+objects from the database hbtn_0e_6_usa"""
 
-if __name__ == "__main__":
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-    with engine.connect() as connection:
-        query = select(State).order_by(State.id.asc())
-        states = connection.execute(query)
-        for state in states:
-            print(f"{state.id}: {state.name}")
 
-    engine.dispose()
+if __name__ == '__main__':
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
