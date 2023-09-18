fe05bba426d4e92cea0ebee518469d7a8973a19d
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-09-17 23:36:11 -0700

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-""" sql alchemy """
-
-from sqlalchemy import (create_engine)
-from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
+"""Start link class to table in database
+"""
 import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, select
 
-
-def allstates():
-    """ list all states """
-
+if __name__ == "__main__":
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                sys.argv[1],
-                sys.argv[2],
-                sys.argv[3]),
-            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+    with engine.connect() as connection:
+        query = select(State).order_by(State.id.asc())
+        states = connection.execute(query)
+        for state in states:
+            print(f"{state.id}: {state.name}")
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
-
-if __name__ == '__main__':
-    allstates()
+    engine.dispose()
