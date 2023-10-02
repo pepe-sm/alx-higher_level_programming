612bc1d4215ef686c6854769b3f71cce06f2e5c5
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-10-02 08:34:46 -0700

    correction

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,12 +3,9 @@
 import requests
 import sys
 
-
-def reqEmail(args, email):
-    """what's my status"""
-    data = {'email': email}
-    x = requests.post(args, data=data)
-    print("{}".format(x.text))
-
 if __name__ == "__main__":
-    reqEmail(sys.argv[1], sys.argv[2])
+    url = sys.argv[1]
+    email = sys.argv[2]
+    params = {"email": email}
+    res = requests.post(url, data=params)
+    print(res.text)
