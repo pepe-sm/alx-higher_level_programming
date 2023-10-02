1063b81752b541a41b1e3938242c6ab3510158d9
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-10-02 08:38:18 -0700

    correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,13 +2,10 @@
 """use request"""
 import requests
 
-
-def FirstRequest():
-    """request"""
-    x = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(x.text)))
-    print("\t- content: {}".format(x.text))
-
 if __name__ == "__main__":
-    FirstRequest()
+    url = "https://alx-intranet.hbtn.io/status"
+    res = requests.get(url)
+    data = res.text
+    resType = type(data)
+    print(f"Body response:\n\t- type: {resType}\n\t\
+- content: {data}")
